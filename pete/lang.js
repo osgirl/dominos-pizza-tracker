c0a1caedb710f48e0a1793b061336484f1e072b2
Nik Nyby <nikolas@gnu.org>
2018-02-10 18:00:19 -0500

    add phaser

---
+++ pete/lang.js
@@ -0,0 +1,22 @@
+var translations = {
+    "US-es": { themeName: "Domino&apos;s Tracker<sup>&reg;</sup> <span>con</span> Pablo el Pizzamaker" },
+    "US-fr": { themeName: "Domino&apos;s Tracker<sup>&reg;</sup> <span>avec</span> Pierre le Pizzamaker" },
+
+    "DO-es": { themeName: "Domino&apos;s Tracker<sup>&reg;</sup> <span>con</span> Pedro el Pizzero" },
+
+    "PR-es": { themeName: "Domino&apos;s Tracker<sup>&reg;</sup> <span>con</span> Pepe el Pizzero" },
+
+    "IT-it": { themeName: "Domino&apos;s Tracker<sup>&reg;</sup> <span>con</span> Pete il pizzaiolo" },
+
+    "PA-es": { themeName: "Domino&apos;s Tracker<sup>&reg;</sup> <span>con</span> Pepe Pizza" },
+
+    "SA-en": { themeName: "Domino&apos;s Tracker<sup>&reg;</sup> <span>with</span> Ahmed the Pizzamaker" },
+    "SA-ar": { themeName: "اتبع طلبات دومينوز مع أحمد صانع البيتزا" },
+
+    "KN-en": { themeName: "Domino&apos;s Tracker<sup>&reg;</sup> <span>with</span> Pedro the Pizzamaker" },
+
+    "SX-en": { themeName: "Domino&apos;s Tracker<sup>&reg;</sup> <span>with</span> Pedro the Pizzamaker" },
+
+    "default": { themeName: "Domino&apos;s Tracker<sup>&reg;</sup> <span>with</span> Pete the Pizzamaker" }
+};
+//# sourceMappingURL=lang.js.map
